e8d8a31988dfa440d3bad7823c0ce7f5dd781ecc
Blackmamba2000 <knking85@gmail.com>
2020-10-13 01:11:31 -0500

    Initial code commit

---
+++ src/main/java/com/otech/king/JavaSpringbootRestApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.otech.king;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootRestApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootRestApiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otech/king/JavaSpringbootRestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otech.king;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootRestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
